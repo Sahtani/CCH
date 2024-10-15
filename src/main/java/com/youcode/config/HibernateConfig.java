69267fec9f4f93f251576f00c96639b7f088f9ec
Sahtani <soumia.sahtani@gmail.com>
2024-10-15 17:22:52 +0100

    Updated pom.xml and HibernateConfig class for Hibernate configuration and necessary dependencies.

--- src/main/java/com/youcode/config/HibernateConfig.java
+++ src/main/java/com/youcode/config/HibernateConfig.java
@@ -1,4 +1,33 @@
 package com.youcode.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
 public class HibernateConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean(name = "sessionFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("com.yourpackage.entities");
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        hibernateProperties.put("hibernate.show_sql", "true");
+        hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
+
+        sessionFactory.setHibernateProperties(hibernateProperties);
+        return sessionFactory;
+    }
 }
