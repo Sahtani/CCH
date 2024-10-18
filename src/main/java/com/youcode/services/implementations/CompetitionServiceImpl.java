65d6d751220b426da70a8dec4bd62b28821a1009
Sahtani <soumia.sahtani@gmail.com>
2024-10-18 11:17:17 +0100

    Implement complete CRUD operations for Competition service

--- src/main/java/com/youcode/services/implementations/CompetitionServiceImpl.java
+++ src/main/java/com/youcode/services/implementations/CompetitionServiceImpl.java
@@ -1,29 +1,36 @@
 package com.youcode.services.implementations;
 
 import com.youcode.entities.Competition;
+import com.youcode.repositories.CompetitionRepository;
 import com.youcode.services.api.CompetitionService;
 
 import java.util.List;
 import java.util.Optional;
 
 public class CompetitionServiceImpl implements CompetitionService {
-    @Override
-    public List<Competition> getAll() {
-        return List.of();
+    private CompetitionRepository competitionRepository;
+
+    public CompetitionServiceImpl(CompetitionRepository competitionRepository) {
+        this.competitionRepository = competitionRepository;
     }
 
     @Override
-    public Optional<Competition> getById(Long aLong) {
-        return Optional.empty();
+    public List<Competition> getAll() {
+        return competitionRepository.findAll();
+    }
+
+    @Override
+    public Optional<Competition> getById(Long id) {
+        return Optional.ofNullable(competitionRepository.findById(id).orElse(null));
     }
 
     @Override
     public Competition save(Competition entity) {
-        return null;
+        return competitionRepository.save(entity);
     }
 
     @Override
-    public void delete(Long aLong) {
-
+    public void delete(Long id) {
+        competitionRepository.deleteById(id);
     }
 }
