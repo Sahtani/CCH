2a5b828b102d525722728e73e1ad39f6eed5e7e3
Sahtani <soumia.sahtani@gmail.com>
2024-10-19 13:38:58 +0100

    Preparation for next steps: - Plan to add DTOs for all domains. - Upcoming modifications for cyclist subscription in competitions.

--- src/main/java/com/youcode/dtos/CyclistDTO.java
+++ src/main/java/com/youcode/dtos/CyclistDTO.java
@@ -1,4 +1,10 @@
 package com.youcode.dtos;
 
 public class CyclistDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String nationality;
+    private String teamName;
 }
